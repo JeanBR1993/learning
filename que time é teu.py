71ddf9734a3cf10d1ca59ab740c5bc3451a7dcbe
JeanBR1993 <66571507+JeanBR1993@users.noreply.github.com>
2020-06-07 17:00:07 -0300

    Add files via upload

---
+++ que time é teu.py
@@ -0,0 +1,16 @@
+# Esse é um programa de estudo de programação em python no que tange à elif
+
+print("Para que time você torce?")
+Time = input()
+if Time == "corinthians":
+    print("Boa meu garoto, esse é o melhor time do Brasil")
+elif Time == "Corinthians":
+    print("Boa meu garoto, esse é o melhor time do Brasil")
+elif Time == "palmeiras":
+    print("Meu deus do céu cara, você não tem vergonha de torcer pra esse lixo não?")
+elif Time == "Palmeiras":
+    print("Meu deus do céu cara, você não tem vergonha de torcer pra esse lixo não?")
+else:
+    print("Nem vale a pena programar mais linhas pra analisar esse time que você digitou ae")
+ImpDesn = input()
+    
